165e648e2860e0e612c7c83cf08325636305d542
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-10 15:48:20 -0400

    Create transaction slice

--- centsight/src/store/index.js
+++ centsight/src/store/index.js
@@ -1,13 +1,24 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 const categoryState = { categories: {}, subcategories: {} };
+const transactionState = { transactions: {} };
 
 const categorySlice = createSlice({
   name: 'categories',
   initialState: categoryState,
   reducers: {},
 });
+const transactionSlice = createSlice({
+  name: 'transactions',
+  initialState: transactionState,
+  reducers: {},
+});
 
-const store = configureStore({ reducer: { category: categorySlice.reducer } });
+const store = configureStore({
+  reducer: {
+    category: categorySlice.reducer,
+    transaction: transactionSlice.reducer,
+  },
+});
 
 export default store;
