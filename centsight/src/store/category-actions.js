c346e8f55a0e77971d32fd1207982777a6760b2d
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-11 11:18:49 -0400

    began supabase tests

--- centsight/src/store/category-actions.js
+++ centsight/src/store/category-actions.js
@@ -1,13 +1,22 @@
-import { supabase } from './../utils/supabase';
+import supabase from './../utils/supabase';
 import { categoryActions } from './category-slice';
 
 export const fetchCategoryData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
-      const { data, error } = await supabase.from('categories').select();
+      const { data: categories, categoryError } = await supabase
+        .from('categories')
+        .select();
 
-      if (error) throw new Error('Failed to fetch category data');
-      return data;
+      const { data: subcategories, subcategoryError } = await supabase
+        .from('subcategories')
+        .select();
+
+      if (categoryError) throw new Error('Failed to fetch category data');
+      if (subcategoryError) throw new Error('Failed to fetch subcategory data');
+
+      //Map subcategories to categories.
+      return categories;
     };
     try {
       const data = await fetchData();
--- centsight/src/store/supabase.test.js
+++ centsight/src/store/supabase.test.js
@@ -1,8 +1,45 @@
-import supabase from './../utils/supabase';
+import { it, describe, expect, test } from 'vitest';
+import supabase from './../../utils/supabase';
+
 /* eslint-disable no-undef */
-describe('Supabase connection', () => {
+describe('Supabase Client', () => {
+  it('should be defined', () => {
+    expect(supabase).toBeDefined();
+  });
+  it('should login user', async () => {
+    const email = 'test@gmail.com';
+    const password = '1234';
+
+    try {
+      const { user, session, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      // Log the response for debugging
+      console.log('Login response:', { user, session, error });
+
+      // Assertions
+      expect(error).toBeNull();
+      expect(user).toBeDefined(); // Check if user object is defined
+      expect(session).toBeDefined(); // Check if session object is defined
+    } catch (error) {
+      console.error('Error logging in:', error.message);
+      throw error; // Re-throw the error to fail the test explicitly
+    }
+  });
   test('data fetched from categories table', async () => {
-    const { data, error } = await supabase.from('categories').select();
-    expect(data);
+    const { data: categories, error } = await supabase
+      .from('categories')
+      .select('*');
+    expect(categories.length).toBeGreaterThan(0);
+    expect(error).toBeNull();
+  });
+  test('data fetched from subcategories table', async () => {
+    const { data: subcategories, error } = await supabase
+      .from('subcategories')
+      .select('*');
+    expect(subcategories.length).toBeGreaterThan(0);
+    expect(error).toBeNull();
   });
 });
