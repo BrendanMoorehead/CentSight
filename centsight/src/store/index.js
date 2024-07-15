c0ca5c619d2c3f07745b7cb38861fff11f33e883
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-14 21:09:02 -0400

    add auth reducer to store

--- centsight/src/store/index.js
+++ centsight/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { categorySlice } from './category-slice';
 import { transactionSlice } from './transaction-slice';
+import { authSlice } from './auth-slice';
 
 export const store = configureStore({
   reducer: {
+    auth: authSlice.reducer,
     category: categorySlice.reducer,
     transaction: transactionSlice.reducer,
   },
