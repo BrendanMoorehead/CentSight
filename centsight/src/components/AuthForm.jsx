7cd0d7c70bbebd6214c6ca6d6a5fd25de3f03ad1
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-15 22:47:08 -0400

    Add login and registration to authform

--- centsight/src/components/AuthForm.jsx
+++ centsight/src/components/AuthForm.jsx
@@ -8,12 +8,28 @@ import {
 } from '@chakra-ui/react';
 import PasswordInput from './PasswordInput';
 import { useState } from 'react';
+import {
+  loginUserWithPassword,
+  registerUserWithPassword,
+} from '../store/auth-actions';
+import { useDispatch } from 'react-redux';
+
 const AuthForm = ({ type, changeForm }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleFormSubmit = () => {
+    if (type === 'signup') {
+      dispatch(registerUserWithPassword(email, password));
+    } else {
+      dispatch(loginUserWithPassword(email, password));
+    }
+  };
 
   return (
     <>
@@ -26,7 +42,9 @@ const AuthForm = ({ type, changeForm }) => {
         />
       </InputGroup>
       <PasswordInput updatePassword={handlePasswordChange} />
-      <Button>{type === 'signup' ? 'Sign up' : 'Login'}</Button>
+      <Button onClick={handleFormSubmit}>
+        {type === 'signup' ? 'Sign up' : 'Login'}
+      </Button>
       <Button onClick={changeForm}>Change Form Type</Button>
     </>
   );
