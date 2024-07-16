75b482d742048dc173ba5e13454dad0fb79f7d1d
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-16 14:45:28 -0400

    add floatbutton with tooltips

--- centsight/src/Pages/DashboardPage.jsx
+++ centsight/src/Pages/DashboardPage.jsx
@@ -4,6 +4,12 @@ import { logoutUser } from '../store/auth-actions';
 import { Button } from '@chakra-ui/react';
 import SideMenu from '../components/SideMenu';
 import Header from '../components/Header';
+import { FloatButton } from 'antd';
+import { TiPlus } from 'react-icons/ti';
+import { MdAccountBalance } from 'react-icons/md';
+import { FaWallet } from 'react-icons/fa';
+import { MdCategory } from 'react-icons/md';
+import { Tooltip } from '@chakra-ui/react';
 const DashboardPage = () => {
   //TODO: User specific data should be fetched here
   const dispatch = useDispatch();
@@ -12,10 +18,29 @@ const DashboardPage = () => {
   };
 
   return (
-    <div>
-      <Header />
-      <SideMenu />
-    </div>
+    <>
+      <div>
+        <Header />
+        <SideMenu />
+      </div>
+      <FloatButton.Group
+        trigger="hover"
+        type="primary"
+        style={{ right: 24 }}
+        icon={<TiPlus />}
+      >
+        <Tooltip label="New category" placement="auto">
+          <FloatButton icon={<MdCategory />} />
+        </Tooltip>
+        <Tooltip label="New account" placement="auto">
+          <FloatButton icon={<MdAccountBalance />} />
+        </Tooltip>
+        <Tooltip label="New transaction" placement="auto">
+          <FloatButton icon={<FaWallet />} />
+        </Tooltip>
+        {/* <FloatButton icon={<CommentOutlined />} /> */}
+      </FloatButton.Group>
+    </>
   );
 };
 
