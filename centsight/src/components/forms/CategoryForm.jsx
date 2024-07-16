6f7a157e542f1cb659b849c306ad1a349d898663
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-16 17:25:57 -0400

    Design updates to category form

--- centsight/src/components/forms/CategoryForm.jsx
+++ centsight/src/components/forms/CategoryForm.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
 import { Segmented, Select } from 'antd';
-
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  FormHelperText,
+  Flex,
+  Spacer,
+} from '@chakra-ui/react';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 const CategoryForm = () => {
+  const categories = useSelector((state) => state.category.categories);
   //TODO: Move setting data to parent
-  const [formData, setFormData] = useState({ type: 'category' });
+  const [formData, setFormData] = useState({ type: 'Category' });
+  const typeString =
+    formData.type.charAt(0).toUpperCase() + formData.type.slice(1) + ' name';
   const handleChange = () => {};
   return (
-    <>
+    <Flex direction="column" gap="6" p="4">
       <Segmented
-        options={['category', 'subcategory']}
+        block
+        size="large"
+        options={['Category', 'Subcategory']}
         onChange={(value) => {
           setFormData({ type: value });
         }}
       />
-      {formData.type === 'subcategory' && (
-        <Select
-          defaultValue="lucy"
-          style={{
-            width: 120,
-          }}
-          onChange={handleChange}
-          options={[
-            {
-              value: 'jack',
-              label: 'Jack',
-            },
-            {
-              value: 'lucy',
-              label: 'Lucy',
-            },
-            {
-              value: 'Yiminghe',
-              label: 'yiminghe',
-            },
-            {
-              value: 'disabled',
-              label: 'Disabled',
-              disabled: true,
-            },
-          ]}
-        />
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input placeholder={typeString}></Input>
+      </FormControl>
+      {formData.type === 'Subcategory' && (
+        <FormControl>
+          <FormLabel>Parent Category</FormLabel>
+          <Select
+            showSearch
+            filterSort={(optionA, optionB) =>
+              (optionA?.label ?? '')
+                .toLowerCase()
+                .localeCompare((optionB?.label ?? '').toLowerCase())
+            }
+            size="large"
+            style={{
+              width: '100%',
+            }}
+            onChange={handleChange}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.name,
+            }))}
+          />
+          <FormHelperText>
+            The category your subcategory will be under
+          </FormHelperText>
+        </FormControl>
       )}
-    </>
+    </Flex>
   );
 };
 
