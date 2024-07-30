4b15482ac5aef38c15f4e23d53de0aeb051168f8
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-07-29 21:46:40 -0400

    account page design and begin click function

--- centsight/src/Pages/AccountsPage.jsx
+++ centsight/src/Pages/AccountsPage.jsx
@@ -2,17 +2,40 @@ import React from 'react';
 import { ScrollShadow } from '@nextui-org/react';
 import { useSelector } from 'react-redux';
 import AccountCard from '../components/accounts/AccountCard';
+import { Button } from '@nextui-org/react';
+import { useState } from 'react';
 const AccountsPage = () => {
   const accounts = useSelector((state) => state.account.accounts);
 
+  const [activeAccount, setActiveAccount] = useState(null);
+
+  const handleAccountClick = (account) => {
+    console.log('click');
+    setActiveAccount(account);
+  };
+
   return (
-    <div className="p-12 h-full">
-      <p className="text-headline text-2xl font-normal pb-6">Accounts</p>
-      <ScrollShadow hideScrollBar className="w-[300px] h-3/4">
-        {accounts.map((account) => (
-          <AccountCard key={account.id} name={account.name} />
-        ))}
-      </ScrollShadow>
+    <div className="p-12 h-[1040px] grid grid-cols-4 gap-24">
+      <div className="col-span-1">
+        <div className="flex justify-between">
+          <p className="text-headline text-2xl font-normal pb-6">Accounts</p>
+          <Button color="primary" variant="ghost">
+            Add
+          </Button>
+        </div>
+        <ScrollShadow hideScrollBar className="h-5/6">
+          {accounts.map((account) => (
+            <AccountCard
+              onClick={handleAccountClick}
+              key={account.id}
+              name={account.name}
+              type={account.type}
+              balance={account.balance}
+            />
+          ))}
+        </ScrollShadow>
+      </div>
+      <div className="col-span-3 border-1"></div>
     </div>
   );
 };
--- centsight/src/components/accounts/AccountCard.jsx
+++ centsight/src/components/accounts/AccountCard.jsx
@@ -1,10 +1,20 @@
 import { Card, CardBody, CardHeader } from '@nextui-org/react';
 
-const AccountCard = ({ name, type, balance }) => {
+const AccountCard = ({ name, type = '', balance, onClick }) => {
   return (
-    <Card className="hover:bg-neutral-700 hover:cursor-pointer">
+    <Card
+      className="hover:bg-neutral-700 hover:cursor-pointer my-4"
+      onClick={onClick}
+    >
       <CardBody>
-        <p className="font-bold">{name}</p>
+        <p className="text-gray-300 text-lg font-headline">{name}</p>
+        <p className="text-4xl font-semibold font-body text-gray-300 pt-4">
+          {'$' + balance.toFixed(2)}
+        </p>
+
+        <p className="text-gray-400 text-md font-headline font-extralight">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </p>
       </CardBody>
     </Card>
   );
