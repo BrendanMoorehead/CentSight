3fa5619f1c4f2b239bdfce2a33fb6e724ee6f2e9
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-11-27 18:03:34 -0500

    calculate new balance function

--- centsight/utils/calculations.js
+++ centsight/utils/calculations.js
@@ -35,3 +35,52 @@ export const calculateTransfer = (accountFrom, accountTo, amount) => {
   const updatedAccountTo = accountTo + amount;
   return { updatedAccountFrom, updatedAccountTo };
 };
+
+/**
+ * Calculates the new account balance after a transaction.
+ *
+ * This function takes a transaction type, an account balance, and amount to adjust the balance by.
+ * It validates the inputs to ensure they are numbers and that the amount is greater than 0.
+ * If any validation fails, an error is thrown.
+ *
+ * @param {string} type - The type of transaction being made on the balance.
+ * @param {number} currentBalance - The current balance of the account before adjustment.
+ * @param {number} amount - The amount to adjust the balance by.
+ * @returns {number} - The new balance after the transaction.
+ *  * @throws {Error} Throws an error if:
+ * - The type is not 'expense' or 'income'.
+ * - the amount or currentBalance are not finite numbers.
+ * - The amount is not greater than 0.
+ *
+ * @example
+ * const result = calculateNewBalance('expense', 100, 50);
+ * result => 50
+ * @example
+ * //Throws an error for invalid type
+ * const result = calculateNewBalance('transfer', 100, 50);
+ */
+export const calculateNewBalance = (type, currentBalance, amount) => {
+  if (type !== 'expense' || type !== 'income') {
+    if (type === 'transfer')
+      throw new Error(
+        'Invalid type: transfer. calculateTransfer() should be used instead.'
+      );
+    else
+      throw new Error('Invalid type: type must be either expense or income.');
+  }
+  if (typeof amount !== 'number' || isNaN(amount) || !isFinite(amount))
+    throw new Error('Invalid amount type: Amount must be a number.');
+  if (
+    typeof currentBalance !== 'number' ||
+    isNaN(currentBalance) ||
+    !isFinite(currentBalance)
+  )
+    throw new Error(
+      'Invalid currentBalance type: currentBalance must be a number.'
+    );
+  else if (amount <= 0)
+    throw new Error('Invalid amount: Amount must be greater than 0.');
+
+  if (type === 'expense') return currentBalance - amount;
+  return currentBalance + amount;
+};
