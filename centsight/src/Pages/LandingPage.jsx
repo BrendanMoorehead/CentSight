80ebb412fcc7ed636dd08ab37c812223d8fcc9e2
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-12-10 18:33:50 -0500

    if authed, navigate to dashboard

--- centsight/src/Pages/LandingPage.jsx
+++ centsight/src/Pages/LandingPage.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from '../store/auth-actions';
 import { Button } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 const LandingPage = () => {
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   const handleLogout = () => {
     dispatch(logoutUser);
   };
+  useEffect(() => {
+    if (auth.user) navigate('/dashboard');
+  }, [auth, navigate]);
 
   return <Button onClick={handleLogout}>Logout</Button>;
 };
